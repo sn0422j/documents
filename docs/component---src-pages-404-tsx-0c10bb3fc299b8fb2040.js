393110d33e37371c17b3fd99bdf9761eff16f384
sn0422j <yotsu.sn.aa@outlook.jp>
2021-08-28 12:19:31 +0000

    delete docs

--- docs/component---src-pages-404-tsx-0c10bb3fc299b8fb2040.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdocuments=self.webpackChunkdocuments||[]).push([[218],{938:function(e,n,t){t.r(n);var u=t(7294);n.default=function(){return u.createElement("body",null,u.createElement("h1",null,"404, page not found!"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-0c10bb3fc299b8fb2040.js.map
